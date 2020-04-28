a578204cb458977ac383523230f26deb6523640d
brokenhoax <brokenhoax@outlook.com>
2020-04-27 21:47:36 -0500

    Added user prompt to enter name

--- js.js
+++ js.js
@@ -1,5 +1,7 @@
+const instructions = document.getElementById('instructions')
 const startButton = document.getElementById('start-btn')
 const nextButton = document.getElementById('next-btn')
+const submitResults = document.getElementById('results-btn')
 const questionContainerElement = document.getElementById ('question-container')
 let shuffledQuestions, currentQuestionIndex
 const questionElement = document.getElementById('question')
@@ -7,15 +9,25 @@ const answerButtonsElement = document.getElementById('answer-buttons')
 const startingMinutes = 2; 
 let time = startingMinutes * 60;
 let timerEl = document.getElementById("tick-tock")
-
-
+// var player;
 
 startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click',() => {
+    answerButtonsElement.classList.remove('hide')
+    questionElement.classList.remove('hide')
+    currentQuestionIndex++
+    setNextQuestion()
+})
+
+submitResults.addEventListener('click', enterScore)
+
 
 function startGame() {
     console.log('Started')
     startButton.classList.add('hide')
+    instructions.classList.add('hide')
     setInterval(timerCounter,1000)
+    setTimeout(timerExpire,120000)
     timerCounter()
     shuffledQuestions = questions.sort(() => Math.random() - .5)
     currentQuestionIndex = 0
@@ -36,38 +48,72 @@ function showQuestion(question) {
         button.classList.add('btn')
         if (answer.correct) {
         button.dataset.correct = answer.correct
-    }
-    button.addEventListener('click', selectAnswer)
-    answerButtonsElement.appendChild(button)
+        }
+        button.addEventListener('click', selectAnswer)
+        answerButtonsElement.appendChild(button)
     })
 }
 
 function resetState() {
+    clearStatusClass(document.body)
     nextButton.classList.add('hide')
     while (answerButtonsElement.firstChild) {
         answerButtonsElement.removeChild(answerButtonsElement.firstChild)
     }
-
 }
 
-function selectAnswer() {
+function selectAnswer(e) {
     const selectedButton = e.target
     const correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonsElement.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+        answerButtonsElement.classList.add('hide')
+        questionElement.classList.add('hide')
+    })
+    if (shuffledQuestions.length > currentQuestionIndex + 1) {
+        nextButton.classList.remove('hide')
+    }       else {
+        answerButtonsElement.classList.add('hide')
+        questionElement.classList.add('hide')
+        submitResults.classList.remove('hide')
+    }
+}
+
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add('correct')
+    } else {
+        element.classList.add('incorrect')
+    }
+}
+
+function clearStatusClass(element) {
+    element.classList.remove('correct')
+    element.classList.remove('incorrect')
 }
 
 const questions = [
     {
         question: 'The "object model" has all but the following:', 
         answers: [
-            {text: 'Properties', correct: true}, 
+            {text: 'Properties', correct: false}, 
             {text: 'Methods', correct: false},
-            {text: 'Signals', correct: false},
+            {text: 'Signals', correct: true},
             {text: 'Events', correct: false},
+        ]
+    }, 
+    {
+        question: 'What is my favorite color?', 
+        answers: [
+            {text: 'Red', correct: false}, 
+            {text: 'Green', correct: false},
+            {text: 'Blue', correct: true},
+            {text: 'Yellow', correct: false},
         ]
     }
 ]
-
-
 
 function timerCounter() {
     const minutes = Math.floor(time / 60);
@@ -77,64 +123,16 @@ function timerCounter() {
     time--;
 }
 
+function timerExpire() {
+    answerButtonsElement.classList.add('hide')
+    questionElement.classList.add('hide')
+    submitResults.classList.remove('hide')
+    timerEl.classList.add('hide')
+}
 
+function enterScore() {
+    var playerName = prompt ("Enter your name to record your score.")
+    localStorage.setItem('User', playerName)
+    console.log(playerName)
+}
 
-
-
-
-// =========================================================================
-
-//     var toDoItems = []
-//     //function to write out list items to DOM
-//     // for loop
-//     // pass the value of the loop into your write out function
-//     $('#readyButton').on('click', function(event) {
-//         var toDo = $("#userInput").val();
-//         writeToDo(toDo);
-//         submitUserInput();
-//     });
-
-
-//     // $('#userInput').keydown(function(event){
-//     //     var keycode = (event.keycode ? event.keycode : event.which);
-//     //     if(keycode == '13'){
-//     //         var toDo = $("#userInput").val();
-//     //         writeToDo(toDo);  
-//     //     }
-//     // });
-
-//     function writeToDo(toDo) {
-//         if (toDo) {
-//             $('#toDoList').append(`
-//                 <div class="alert alert-secondary taskList" id="toDoItem" role="toDoItem">
-//                     <div class="row">
-//                         <div class="col-10">
-//                             <p id="toDoText">
-//                                 ${toDo}
-//                             </p>
-//                         </div>
-//                         <div class="col-2 ">
-//                             <button type="button" class="btn deletebtn btn-success float-right" id="${toDo}">Complete</button>
-//                         </div>
-//                     </div>
-//                 </div>`
-//             );
-//     }
-//     }
-
-//     function submitUserInput(){
-//         toDoItems.push($('#userInput').val());
-//         console.log(toDoItems);
-
-//     }
-
-//     $(document).on('click', 'button.deletebtn', function (toDo) {
-//         $(this).closest('.taskList').remove();
-//         return false;
-
-//     });
-
-//     // function deleteToDo(){
-//     //     $('#toDO').on('click', function(event) {
-
-//     // });
